09f8cf170152ea184a2a6628888178e1d188f849
Ole Bulbuk <ole@bulbuk.de>
2021-03-09 22:54:24 +0100

    Fix markdown bug

--- stat/stat.go
+++ stat/stat.go
@@ -34,8 +34,8 @@ func Generate(startPkg string, depMap data.DependencyMap) string {
 
 Start package - ` + startPkg + `
 
-max score - the sum of the packages hidden from user packages",
-min score - the packages hidden from all user packages combined",
+* max score - The sum of the packages hidden from user packages.
+* min score - The packages hidden from all user packages combined.
 
 | package | type | direct deps | all deps | users | max score | min score |
 | :- | :-: | -: | -: | -: | -: | -: |
